619f5e466fcf401dfc0d9aed2dba6d38088c59b7
Robert Meredith <64670585+robertmeredith@users.noreply.github.com>
2023-09-14 13:04:09 +1000

    edited server for deploying to heroku

--- server.js
+++ server.js
@@ -85,11 +85,10 @@ app.use('/api/v1/vendors', vendorRouter)
 app.use('/api/v1/settings', userSettingsRouter)
 
 // DEPLOYMENT - after trying above routes, serve index.html file
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirName, 'client/build', 'index.html'))
-// })
-// app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirName, 'client/build', 'index.html'))
+})
+app.use(express.static('client/build'))
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
